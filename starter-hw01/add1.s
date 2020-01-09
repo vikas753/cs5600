236572160d8a8a72d9a2e59762cf66aedd14f659
vikas753 <vvikas1996@gmail.com>
2020-01-08 23:39:25 -0500

    Fix the add1 for i386 properly by using offset of ebp to store function params

--- starter-hw01/add1.s
+++ starter-hw01/add1.s
@@ -4,11 +4,14 @@
 # add1 function , adds 1 to a long integer
 # as the prototype expects the value to be a 
 # long we are not worried about integer overflows
-# in i386 processor pushed value is either in %eax or at 4 or 8 offset
-# of ebp register , in which case we would be incrementing eax itself
+# in i386 processor pushed value is either in %eax or at 8 offset
+# of ebp register . Using eax is quite risky as any non related function 
+# call before can modify it;s value thereby serving the function with garbage
+# thus using 8(%ebp) which as first local variable is worth it 
 add1:
   enter $0 , $0
-  addl $1 ,  %eax
+  mov 8(%ebp) ,  %eax
+  inc %eax
   leave
   ret
 
